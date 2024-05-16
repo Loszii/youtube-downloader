b3ace88ee43db4ad3eadb3f87699273df19a6d63
Loszi <jiozzi11@gmail.com>
2024-05-16 18:33:30 -0400

    made playlists and multi-vid go into their own directory, improved name seperating from video description, and added simpler program responses

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import subprocess
 import pickle
 import json
 
-#idea: make playlist and multi-song download to own dir
+#todo: make multi video work for videos over 1 hour
 
 def main():
     #gets path, url from user, and converts the video/s to .mp3
@@ -14,18 +14,21 @@ def main():
         if mode == "p":
             playlist_url = input("url:\n")
             try:
+                print("starting download..")
                 playlist_downloader(path, playlist_url)
             except Exception as e:
                 print(e)
         elif mode == "v":
             video_url = input("url:\n")
             try:
+                print("starting download..")
                 vid_downloader(path, video_url) 
             except Exception as e:
                 print(e)
         elif mode == "m":
             video_url = input("url:\n")
             try:
+                print("starting download..")
                 multi_downloader(path, video_url)
             except Exception as e:
                 print(e)
@@ -33,13 +36,29 @@ def main():
             print("Enter valid mode")
 
 def vid_downloader(path, url):
-    subprocess.run(f"yt-dlp -f \"ba*\" \"{url}\" -o \"%(title)s.%(ext)s\" -P \"temp\" --no-playlist", shell=True)
+    #downloads a single video
+    subprocess.run(f"yt-dlp -f \"ba*\" \"{url}\" -o \"%(title)s.%(ext)s\" -P \"temp\" --no-playlist -q --no-warnings", shell=True)
     to_mp3(path)
     os.rmdir(f"temp")
 
 def playlist_downloader(path, url):
-    subprocess.run(f"yt-dlp -f \"ba*\" \"{url}\" -o \"%(title)s.%(ext)s\" -P \"temp\"", shell=True)
-    to_mp3(path)
+    #downloads all videos in a playlist
+
+    #first get the title for new path
+    subprocess.run(f"yt-dlp -I 0 --write-info-json \"{url}\" -P \"temp\" -q --no-warnings")
+    playlist_info = os.listdir("temp")[0]
+    playlist_name = format_name(json.load(open(f"temp/{playlist_info}", encoding="utf-8", errors="ignore"))["title"]) #gets playlist name from json and formats to become dir
+    os.remove(f"temp/{playlist_info}")
+    if "\\" in path:
+        new_path = path + playlist_name + "\\"
+    else:
+        new_path = path + playlist_name + "/" #new folder with playlist name
+
+    #make new folder at new_path
+    os.mkdir(new_path)
+
+    subprocess.run(f"yt-dlp -f \"ba*\" \"{url}\" -o \"%(title)s.%(ext)s\" -P \"temp\" --yes-playlist -q --no-warnings", shell=True)
+    to_mp3(new_path)
     os.rmdir("temp")
 
 def to_mp3(path):
@@ -47,6 +66,7 @@ def to_mp3(path):
     files = os.listdir("temp")
     for title in files:
         new_title = os.path.splitext(title)[0] + ".mp3" #changes name to new extension
+        print(f"downloading: {new_title}")
         subprocess.run(f"ffmpeg -i \"temp/{title}\" -vn -q:a 0 -map a \"{path}{new_title}\" -loglevel 0", shell=True)
         #removing temp file
         os.remove(f"temp/{title}")
@@ -64,7 +84,7 @@ def get_path():
             #putting path into correct format
             new_path = input("enter path below:\n").replace("\"", "") #removes quotes from copying path via file explorer
             if new_path[len(new_path)-1] != "/" and new_path[len(new_path)-1] != "\\": #add final slash for compatability with to_mp3() func
-                if ("\\" in new_path):
+                if "\\" in new_path:
                     new_path += "\\"
                 else:
                     new_path += "/"
@@ -103,11 +123,7 @@ def to_multi_mp3(path, song_data):
     #takes one song and splits it into multiple times mp3s
     #song_data is a dictionary with title being key and value being the time-stamp
 
-    files = os.listdir("temp")
-    if ".info.json" in files[0]:
-        title = files[1]
-    else:
-        title = files[0]
+    title = os.listdir("temp")[0]
 
     new_title = os.path.splitext(title)[0] + ".mp3" #changes name to new extension
     subprocess.run(f"ffmpeg -i \"temp/{title}\" -vn -q:a 0 -map a \"temp/{new_title}\" -loglevel 0", shell=True)
@@ -117,9 +133,10 @@ def to_multi_mp3(path, song_data):
     #path/new_title is location of parent mp3
     for i in song_data:
         name = i + ".mp3"
+        print(f"downloading: {name}")
         start = song_data[i][0]
         duration = song_data[i][1]
-        if duration == None:
+        if duration == None: #final song
             subprocess.run(f"ffmpeg -i \"temp/{new_title}\" -ss {start} -c copy \"{path}{name}\" -loglevel 0", shell=True)
         else:
             subprocess.run(f"ffmpeg -i \"temp/{new_title}\" -ss {start} -t {duration} -c copy \"{path}{name}\" -loglevel 0", shell=True)
@@ -131,23 +148,31 @@ def to_multi_mp3(path, song_data):
 
 def multi_downloader(path, url):
     #function for a video composed of multiple songs that have time stamps in the description
-    subprocess.run(f"yt-dlp -f \"ba*\" \"{url}\" -o \"%(title)s.%(ext)s\" -P \"temp\" --no-playlist --write-info-json", shell=True)
+    subprocess.run(f"yt-dlp -f \"ba*\" \"{url}\" -o \"%(title)s.%(ext)s\" -P \"temp\" --no-playlist --write-info-json -q --no-warnings", shell=True)
 
-    #get time stamps here
-    song_data = get_time_stamps()
-
-    to_multi_mp3(path, song_data)
-    os.rmdir(f"temp")
-
-def get_time_stamps():
-    #finds json in temp and gets description to decode time stamps and title
+    #make new folder with name of video for dir
     files = os.listdir("temp")
     for i in files:
         if ".info.json" in i:
             data = i
             break
+    video_name = format_name(json.load(open(f"temp/{data}", encoding="utf-8", errors="ignore"))["title"])
+    if "\\" in path:
+        new_path = path + video_name + "\\"
+    else:
+        new_path = path + video_name + "/" #new folder with playlist name
+    os.mkdir(new_path)
 
-    desc = json.load(open(f"temp/{data}", encoding="utf-8"))["description"]
+    #get time stamps here
+    song_data = get_time_stamps(data)
+
+    os.remove(f"temp/{data}")
+    to_multi_mp3(new_path, song_data)
+    os.rmdir(f"temp")
+
+def get_time_stamps(data):
+    #finds json in temp and gets description to decode time stamps and title
+    desc = json.load(open(f"temp/{data}", encoding="utf-8", errors="ignore"))["description"]
     split_desc = desc.split("\n")
 
     times = []
@@ -176,12 +201,24 @@ def get_time_stamps():
         if len(temp_data) > 0: #compute time diff and add to last song for length
             temp_data[-1] += [length]
         prev_stamp = stamp
-        name = format_name(song.replace(stamp, ""))
+        name = song.replace(stamp, "")
         temp_data += [[name, stamp]] #add current song
 
     temp_data[-1] += [None] #last one is none since it will go to the ends of the file
 
     for i in range(len(temp_data)):
+
+        #remove second time stamp if still in name:
+        name = temp_data[i][0]
+        if ":" in name:
+            index = name.find(":")
+            if len(name) - index > 2 and index > 0:
+                if name[index-1] in nums and name[index+1] in nums and name[index+2] in nums:
+                    time_to_remove = name[index-2:index+3]
+                    name = name.replace(time_to_remove, "")
+
+        temp_data[i][0] = format_name(name.replace("-", "").strip())
+
         song_data[temp_data[i][0]] = (temp_data[i][1], temp_data[i][2]) #add to dict
 
     return song_data
