c391453cd6ff6bf52e98c9b6fa1183d9a06470a0
Loszi <jiozzi11@gmail.com>
2024-05-16 22:50:17 -0400

    improved code, now uses built in yt-dlp for multi-video. also fixed some name formatting issues

--- main.py
+++ main.py
@@ -4,8 +4,6 @@ import subprocess
 import pickle
 import json
 
-#todo: make multi video work for videos over 1 hour
-
 def main():
     #gets path, url from user, and converts the video/s to .mp3
     path = get_path()
@@ -39,27 +37,51 @@ def vid_downloader(path, url):
     #downloads a single video
     subprocess.run(f"yt-dlp -f \"ba*\" \"{url}\" -o \"%(title)s.%(ext)s\" -P \"temp\" --no-playlist -q --no-warnings", shell=True)
     to_mp3(path)
-    os.rmdir(f"temp")
 
 def playlist_downloader(path, url):
     #downloads all videos in a playlist
 
-    #first get the title for new path
+    #first get the title for new dir
     subprocess.run(f"yt-dlp -I 0 --write-info-json \"{url}\" -P \"temp\" -q --no-warnings")
     playlist_info = os.listdir("temp")[0]
     playlist_name = format_name(json.load(open(f"temp/{playlist_info}", encoding="utf-8", errors="ignore"))["title"]) #gets playlist name from json and formats to become dir
     os.remove(f"temp/{playlist_info}")
-    if "\\" in path:
-        new_path = path + playlist_name + "\\"
-    else:
-        new_path = path + playlist_name + "/" #new folder with playlist name
+
+    new_path = path_join(path, playlist_name)
 
     #make new folder at new_path
     os.mkdir(new_path)
 
+    #download all playlist videos
     subprocess.run(f"yt-dlp -f \"ba*\" \"{url}\" -o \"%(title)s.%(ext)s\" -P \"temp\" --yes-playlist -q --no-warnings", shell=True)
     to_mp3(new_path)
-    os.rmdir("temp")
+
+def multi_downloader(path, url):
+    #function for a video composed of multiple songs that have time stamps in the description
+
+    #download all chapters and original video
+    subprocess.run(f"yt-dlp -f \"ba*\" \"{url}\" -o \"%(title)s.%(ext)s\" --split-chapters -o \"chapter:%(section_title)s.%(ext)s\" -P \"temp\" --write-info-json --no-playlist -q --no-warnings", shell=True)
+    
+    files = os.listdir("temp")
+    for i in files:
+        if ".info.json" in i:
+            json_file = i
+            data = json.load(open(f"temp/{i}", encoding="utf-8", errors="ignore"))
+
+    non_split_file = format_name(data["title"]) #get original video title and format
+    non_split_ext = data["ext"]
+    
+    to_remove = os.path.splitext(json_file)[0][:-5] + "." + non_split_ext #using json file to get post-window filename format to delete
+
+    os.remove(f"temp/{to_remove}")
+    os.remove(f"temp/{json_file}")
+
+    new_path = path_join(path, non_split_file)
+
+    #make new folder at new_path
+    os.mkdir(new_path)
+
+    to_mp3(new_path)
 
 def to_mp3(path):
     #converts video from temp to .mp3 format with ffmpeg
@@ -67,9 +89,10 @@ def to_mp3(path):
     for title in files:
         new_title = os.path.splitext(title)[0] + ".mp3" #changes name to new extension
         print(f"downloading: {new_title}")
-        subprocess.run(f"ffmpeg -i \"temp/{title}\" -vn -q:a 0 -map a \"{path}{new_title}\" -loglevel 0", shell=True)
+        subprocess.run(f"ffmpeg -loglevel 0 -i \"temp/{title}\" -vn -q:a 0 -map a \"{path}{new_title}\"", shell=True) #to mp3
         #removing temp file
         os.remove(f"temp/{title}")
+    os.rmdir(f"temp")
 
 
 def get_path():
@@ -119,131 +142,13 @@ def print_paths(data):
         else:
             print(str(i) + ". " + data[i])
 
-def to_multi_mp3(path, song_data):
-    #takes one song and splits it into multiple times mp3s
-    #song_data is a dictionary with title being key and value being the time-stamp
-
-    title = os.listdir("temp")[0]
-
-    new_title = os.path.splitext(title)[0] + ".mp3" #changes name to new extension
-    subprocess.run(f"ffmpeg -i \"temp/{title}\" -vn -q:a 0 -map a \"temp/{new_title}\" -loglevel 0", shell=True)
-    #removing old format file in temp
-    os.remove(f"temp/{title}")
-
-    #path/new_title is location of parent mp3
-    for i in song_data:
-        name = i + ".mp3"
-        print(f"downloading: {name}")
-        start = song_data[i][0]
-        duration = song_data[i][1]
-        if duration == None: #final song
-            subprocess.run(f"ffmpeg -i \"temp/{new_title}\" -ss {start} -c copy \"{path}{name}\" -loglevel 0", shell=True)
-        else:
-            subprocess.run(f"ffmpeg -i \"temp/{new_title}\" -ss {start} -t {duration} -c copy \"{path}{name}\" -loglevel 0", shell=True)
-
-    #delete the files after this
-    files = os.listdir("temp")
-    for title in files:
-        os.remove(f"temp/{title}")
-
-def multi_downloader(path, url):
-    #function for a video composed of multiple songs that have time stamps in the description
-    subprocess.run(f"yt-dlp -f \"ba*\" \"{url}\" -o \"%(title)s.%(ext)s\" -P \"temp\" --no-playlist --write-info-json -q --no-warnings", shell=True)
-
-    #make new folder with name of video for dir
-    files = os.listdir("temp")
-    for i in files:
-        if ".info.json" in i:
-            data = i
-            break
-    video_name = format_name(json.load(open(f"temp/{data}", encoding="utf-8", errors="ignore"))["title"])
+def path_join(path, ext):
+    #custom path joiner
     if "\\" in path:
-        new_path = path + video_name + "\\"
-    else:
-        new_path = path + video_name + "/" #new folder with playlist name
-    os.mkdir(new_path)
-
-    #get time stamps here
-    song_data = get_time_stamps(data)
-
-    os.remove(f"temp/{data}")
-    to_multi_mp3(new_path, song_data)
-    os.rmdir(f"temp")
-
-def get_time_stamps(data):
-    #finds json in temp and gets description to decode time stamps and title
-    desc = json.load(open(f"temp/{data}", encoding="utf-8", errors="ignore"))["description"]
-    split_desc = desc.split("\n")
-
-    times = []
-    nums = "0123456789"
-    for i in range(len(split_desc)):
-        if ":" in split_desc[i]:
-            #find index of :
-            index = split_desc[i].find(":")
-
-            #formatting
-            if index > 0 and split_desc[i][index-1] in nums and split_desc[i][index+1] in nums and split_desc[i][index+2] in nums:
-                if index == 1:
-                    split_desc[i] = "0" + split_desc[i]
-                elif split_desc[i][index-2] not in nums:
-                    split_desc[i] = split_desc[i][:index-1] + "0" + split_desc[i][index-1:]
-                times.append(split_desc[i])
-
-    song_data = {}
-    temp_data = [] #using this to computer delta time and add to prev
-    #temp data of format [[name, start, duration], ...]
-    prev_stamp = "00:00"
-    for song in times:
-        index = song.find(":")
-        stamp = song[index-2:index+3]
-        length = time_difference(prev_stamp, stamp)
-        if len(temp_data) > 0: #compute time diff and add to last song for length
-            temp_data[-1] += [length]
-        prev_stamp = stamp
-        name = song.replace(stamp, "")
-        temp_data += [[name, stamp]] #add current song
-
-    temp_data[-1] += [None] #last one is none since it will go to the ends of the file
-
-    for i in range(len(temp_data)):
-
-        #remove second time stamp if still in name:
-        name = temp_data[i][0]
-        if ":" in name:
-            index = name.find(":")
-            if len(name) - index > 2 and index > 0:
-                if name[index-1] in nums and name[index+1] in nums and name[index+2] in nums:
-                    time_to_remove = name[index-2:index+3]
-                    name = name.replace(time_to_remove, "")
-
-        temp_data[i][0] = format_name(name.replace("-", "").strip())
-
-        song_data[temp_data[i][0]] = (temp_data[i][1], temp_data[i][2]) #add to dict
-
-    return song_data
-
-def time_difference(start, end):
-    #takes in start and end time of format ##:## and returns difference
-    end_mins = int(end[:2])
-    start_mins = int(start[:2])
-    total_mins = end_mins - start_mins
-
-    end_secs = int(end[3:])
-    start_secs = int(start[3:])
-    total_secs = end_secs - start_secs
-    if total_secs < 0:
-        total_mins -= 1
-        total_secs += 60
-
-    total_mins = str(total_mins)
-    total_secs = str(total_secs)
-
-    if len(total_mins) == 1:
-        total_mins = "0" + total_mins
-    if len(total_secs) == 1:
-        total_secs = "0" + total_secs
-    return str(total_mins) + ":" + str(total_secs)
+        new_path = path + ext + "\\"
+    else: 
+        new_path = path + ext + "/"
+    return new_path
 
 def format_name(name):
     #takes in name of song and makes sure there are no illegal characters for saving to pc
